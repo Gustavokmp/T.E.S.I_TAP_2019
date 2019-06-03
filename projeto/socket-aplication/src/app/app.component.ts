ba1bf091c3ea6ecc3348cc8fa4233cb552282926
Gustavo Pires <gustavomp.ti@gmail.com>
2019-06-02 21:28:17 -0300

    tela login

--- projeto/socket-aplication/src/app/app.component.ts
+++ projeto/socket-aplication/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
   title = 'socket-app';
+  isLogin = false;
+  username: string  = '';
+  password: string  = '';
+  registro = {
+    username:'',
+    password:'',
+    email:''
+  }
+
+  login(){
+    console.log(this.username, this.password);
+    this.isLogin = true;
+    
+  }
+  registrar(){
+    console.log(this.registro);
+  }
+  sair(){
+    this.isLogin = false;
+  }
+
 }
